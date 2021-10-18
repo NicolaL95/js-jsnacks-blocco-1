f7870f5e0e554275e0f6d5181dfe0ad144121103
NicolaL95 <nicola.lilla@gmail.com>
2021-10-18 16:41:14 +0200

    Inizio quinto esercizio

--- main.js
+++ main.js
@@ -47,9 +47,39 @@ if (es_sel == 3) {
     /* Esercizio 3 */
     let sum = 0;
     for (let index = 0; index <= 9; index++) {
-        let element = prompt(`Inserisci il ${index + 1}° numero`)
+        let element = parseInt(prompt(`Inserisci il ${index + 1}° numero`));
+        console.log(element)
+        /* while (element = isNaN) {
+            element = parseInt(prompt(`non hai inserito un numero, inserisci il ${index + 1}° numero`));
+            if ((element == isNaN) == false) {
+                break;
+            }
+        } */
         sum += element;
-        console.log(element, sum);
     }
-
 }
+
+if (es_sel == 4) {
+    /* Esercizio 4 */
+    const invited_list = ["Stefano", "Andrea", "Nicola", "Lorenzo", "Matteo", "Diego", "Luigi", "Simone"];
+    let user_invited = prompt("inserisci il tuo nome");
+    for (let index = 0; index <= invited_list.length; index++) {
+        if (user_invited == invited_list[index].toLowerCase()) {
+            window.alert(`${invited_list[index]} è sulla lista!`);
+        }
+    }
+}
+
+if (es_sel == 5) {
+    /* Esercizio 5 */
+    const odd_list = [];
+    for (let index = 0; index <= 6; index++) {
+        let num_chk = prompt("inserisci un numero")
+        console.log(num_chk)
+        if (num_chk % 2 == 1) {
+            odd_list.push = num_chk;
+        }
+    }
+    console.log(odd_list)
+}
+
